36db3c41dc9abd066738dabb64850c15dd3077af
Kusal95 <kusalmarsh95@gmail.com>
2024-10-10 08:34:42 +0530

    Reservation email sending process added

--- app/Mail/ReservationMail.php
+++ app/Mail/ReservationMail.php
@@ -4,19 +4,13 @@ namespace App\Mail;
 
 use App\Models\Reservation;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
 class ReservationMail extends Mailable
 {
     use Queueable, SerializesModels;
 
-    /**
-     * Create a new message instance.
-     */
     public $reservation;
 
     public function __construct(Reservation $reservation)
@@ -24,34 +18,9 @@ class ReservationMail extends Mailable
         $this->reservation = $reservation;
     }
 
-
-    /**
-     * Get the message envelope.
-     */
-    public function envelope(): Envelope
+    public function build()
     {
-        return new Envelope(
-            subject: 'Reservation Request',
-        );
-    }
-
-    /**
-     * Get the message content definition.
-     */
-    public function content(): Content
-    {
-        return new Content(
-            view: 'mail.reservation-mail',
-        );
-    }
-
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
-     */
-    public function attachments(): array
-    {
-        return [];
+        return $this->subject('Request a Guide Reservation')
+            ->view('mail.reservation-mail');
     }
 }
--- resources/views/pages/app.blade.php
+++ resources/views/pages/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <title>Sumith Tours</title>
     <meta charset="utf-8">
+    <link rel="shortcut icon" type="x-icon" href="{{ asset("images/sumithtours.jpg") }}">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
@@ -30,7 +31,6 @@
         </div>
     </div>
 </div>
-<!-- ***** Preloader End ***** -->
 
 <!-- ***** Header Area Start ***** -->
 <header class="header-area header-sticky">
@@ -40,13 +40,14 @@
                 <nav class="main-nav">
                     <!-- ***** Logo Start ***** -->
                     <a href="/" class="logo">
-                        <img src="{{ asset("images/destination.png") }}" alt="">
+                        <img src="{{ asset("images/sumithtours.jpg") }}" alt="">
+{{--                        <img src="{{ asset("images/destination.png") }}" alt="">--}}
                     </a>
-                    <!-- ***** Logo End ***** -->
-                    <!-- ***** Menu Start ***** -->
+
                     <ul class="nav">
                         <li><a href="/">Home</a></li>
                         <li><a href="/about">About</a></li>
+                        <li><a href="/gallery">Gallery</a></li>
                         <li><a href="deals.html">Services</a></li>
                         <li><a href="/itinerary">Itinerary</a></li>
                         <li><a href="/reservation">Reservation</a></li>
